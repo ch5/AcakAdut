899da3667d5fb42186fb9adc96f282a8fbf939d7
ch5 <kholis13@gmail.com>
2017-01-25 10:03:49 +0700

    add my new photos

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -38,8 +38,6 @@ class ArticlesController < ApplicationController
   # POST /articles.json
   def create
     @article = Article.new(article_params)
-    @article.category_id = params[:category_id]
-
     respond_to do |format|
       if @article.save
         format.html { redirect_to @article, notice: 'Article was successfully created.' }
--- config/routes.rb
+++ config/routes.rb
@@ -2,11 +2,12 @@ Rails.application.routes.draw do
   # mount Bootsy::Engine => '/bootsy', as: 'bootsy'
   root 'articles#index'
   get 'tags/:tag', to: 'articles#index', as: :tag
-  get 'categories/:category', to: 'articles#index', as: :category
+  get 'by_category/:category', to: 'articles#index', as: :category
   # get 'articles/by_category/:category', to: 'articles#by_categories', as: :article
 
   # resources :tags, only: [:index, :show]
   resources :articles
+  resources :categories
 
   # resources :articles do
   #   collection do
@@ -14,7 +15,6 @@ Rails.application.routes.draw do
   #   end
   # end
 
-  resources :categories
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
